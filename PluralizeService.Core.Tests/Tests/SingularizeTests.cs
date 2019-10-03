df063e105b767b19656f524c95f72236d91cd5f6
will.blackburn <will.blackburn@cubic-interactive.com>
2019-10-03 20:24:13 +0100

    Added XUnit tests to prove everything works though singleton class created

---
+++ PluralizeService.Core.Tests/Tests/SingularizeTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace PluralizeService.Core.Tests.Tests
+{
+    public class SingularizeTests
+    {
+        [Theory]
+        [InlineData("Emails", "Email")]
+        [InlineData("companies", "company")]
+        [InlineData("Projects", "Project")]
+        [InlineData("Containers", "Container")]
+        [InlineData("buses", "bus")]
+        public void ShouldSingularizeWord(string word, string expected)
+        {
+            // Arrange
+            string actual = PluralizationProvider.Singularize(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
