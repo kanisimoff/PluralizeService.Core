17647b071db0526289b5d580032d3c3a5f5d8e40
Lee Dawson <lee.t.dawson@gmail.com>
2020-03-28 14:42:41 +0000

    * Add IsPlural() and IsSingular() public methods to singleton provider.

---
+++ PluralizeService.Core.Tests/Tests/IsPluralTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace PluralizeService.Core.Tests
+{
+    public class IsPluralTests
+    {
+        [Theory]
+        [InlineData("Email", false)]
+        [InlineData("Emails", true)]
+        [InlineData("company", false)]
+        [InlineData("companies", true)]
+        [InlineData("Crisis", false)]
+        [InlineData("Crises", true)]
+        [InlineData("Fungus", false)]
+        [InlineData("Fungi", true)]
+        [InlineData("bus", false)]
+        [InlineData("buses", true)]
+        public void WordIsPlural(string word, bool expected)
+        {
+            // Arrange
+            bool actual = PluralizationProvider.IsPlural(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ PluralizeService.Core.Tests/Tests/IsSingularTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace PluralizeService.Core.Tests
+{
+    public class IsSingularTests
+    {
+        [Theory]
+        [InlineData("Email", true)]
+        [InlineData("Emails", false)]
+        [InlineData("company", true)]
+        [InlineData("companies", false)]
+        [InlineData("Crisis", true)]
+        [InlineData("Crises", false)]
+        [InlineData("Fungus", true)]
+        [InlineData("Fungi", false)]
+        [InlineData("bus", true)]
+        [InlineData("buses", false)]
+        public void WordIsSingular(string word, bool expected)
+        {
+            // Arrange
+            bool actual = PluralizationProvider.IsSingular(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
